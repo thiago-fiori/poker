e0ae05bff08983a1731a93e6ae51b186a140da2e
Thiago Abreu Fiori <thiago.fiori@estudante.ufla.br>
2024-01-15 18:55:50 -0300

    Cria botões de adicionar e remover jogador

--- app.vue
+++ app.vue
@@ -1,7 +1,53 @@
 <template>
-  <div>
+  <div class="sala">
     <Mesa>
-      <Assento><Contador></Contador></Assento>
+      <Assento v-for="Jogador in Array(jogadores)"></Assento>
     </Mesa>
+
+    <footer>
+      <button @click="adicionarJogador">Adicionar Jogador</button>
+      <button @click="removerJogador">Remover Jogador</button>
+    </footer>
+
   </div>
 </template>
+
+<script setup>
+  const jogadores = useState("jogadores", () =>1);
+  const adicionarJogador = function(){
+    console.log("Botão apertado");
+    jogadores.value++;
+    //console.log(jogadores);
+  }
+
+  const removerJogador = function(){
+    console.log("Botão apertado");
+    
+    if(jogadores.value > 0){
+      jogadores.value--;
+    }
+    //console.log(jogadores);
+  }
+</script>
+
+<style scoped>
+  body{
+    padding: 0;
+  }
+
+  footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  button{
+  
+  }
+
+  .sala{
+    height: 90vh;
+    background-color: brown;
+    margin: 0;
+  }
+</style>
